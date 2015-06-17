270a6e8ea72c727b9da4fe0b2a2fc6e104f35279
Piotr Kaczmarczyk <piokaczm@gmail.com>
2015-06-17 21:30:23 +0200

    add main_pages_controller test, dynamic titles, and improve footer and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'main_pages#home'
 
-  get 'main_pages/help'
-
-  get 'main_pages/about'
-
-  get 'main_pages/contact'
+  get 'pomoc' => 'main_pages#help'
+  get 'o_nas' => 'main_pages#about'
+  get 'kontakt' => 'main_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/main_pages_controller_test.rb
+++ test/controllers/main_pages_controller_test.rb
@@ -4,21 +4,25 @@ class MainPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "KGO | Koleżeńska Giełda Ostrokołowa"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Pomoc | KGO"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "O nas | KGO"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Kontakt | KGO"
   end
 
 end

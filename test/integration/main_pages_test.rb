2f67c8793db15c7d9bc376c555f9f64d07b04139
Piotr Kaczmarczyk <piokaczm@gmail.com>
2015-06-18 10:32:29 +0200

    Add main_pages integration test

---
+++ test/integration/main_pages_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class MainPagesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  test "should have links" do
+    get root_path
+    assert_template 'main_pages/home'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", o_nas_path
+    assert_select "a[href=?]", kontakt_path
+    assert_select "a[href=?]", pomoc_path
+  end
+end

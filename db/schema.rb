8682000903f610b4789e6bc34a6831841b6ac7a7
Piotr Kaczmarczyk <piokaczm@gmail.com>
2015-06-18 13:34:39 +0200

    add user validations and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618101506) do
+ActiveRecord::Schema.define(version: 20150618103140) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20150618101506) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "city"
+    t.string   "zip"
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,109 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @user = User.new(username: "kaczmuch", name: "Piotr", surename: "Kaczmarczyk", email: "piokaczm@gmail.com", address: "Jagiełły 98", city: "Tychy", zip: "43-100", password: "foobar", password_confirmation: "foobar")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "username should be present" do
+    @user.username = "  "
+    assert_not @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
+  
+  test "surename should be present" do
+    @user.surename = "  "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+  
+  test "address should be present" do
+    @user.address = "  "
+    assert_not @user.valid?
+  end
+  
+  test "city should be present" do
+    @user.city = "  "
+    assert_not @user.valid?
+  end
+  
+  test "zip should be present" do
+    @user.city = "  "
+    assert_not @user.valid?
+  end
+  
+  test "password should be present" do
+    @user.password = @user.password_confirmation = "  "
+    assert_not @user.valid?
+  end
+  
+  test "username shouldn't be too long" do
+    @user.username = "a" * 21
+    assert_not @user.valid?
+  end
+  
+  test "zip should have correct format" do
+    @user.zip = "43430"
+    assert_not @user.valid?
+  end
+  
+  test "password should have valid length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+  
+  test "password should be confirmed" do
+    @user.password_confirmation = "foobal"
+    assert_not @user.valid?
+  end
+  
+  test "email should have valid format" do
+    
+     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?
+    end
+    
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?
+    end
+    
+  end
+  
+  test "email should be unique" do
+    user_dup = @user.dup
+    @user.save
+    user_dup.username = "cipsko"
+    user_dup.email = @user.email.upcase
+    assert_not user_dup.valid?
+  end
+  
+  test "username should be unique" do
+    user_dup = @user.dup
+    @user.save
+    user_dup.email = "ppapa@papa.com"
+    user_dup.username = @user.username.upcase
+    assert_not user_dup.valid?
+  end
+    
+  
 end

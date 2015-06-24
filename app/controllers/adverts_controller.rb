40a8d99b20d55ed865958f39543d755535e2814f
Piotr Kaczmarczyk <piokaczm@gmail.com>
2015-06-24 23:27:11 +0200

    add login check before getting to adverts#new ,edit, create

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -1,5 +1,8 @@
 class AdvertsController < ApplicationController
+  
+  before_action :is_logged_in, only: [:new, :edit, :create]
   def new
+    @advert = Advert.new
   end
 
   def show
@@ -25,6 +28,17 @@ class AdvertsController < ApplicationController
   
   private
   
+  def advert_params
+    params.require(:advert).permit(:title, :content, :price, :wojewodztwo, :user_id, :new, :size1, :size2, :city, :category)
+  end
 
-   
+  
+   def is_logged_in
+      unless logged_in?
+        store_location
+        flash["danger"] = "Aby wyświetlić tę stronę należy się zalogować"
+        redirect_to login_path
+      end
+    end
+  
 end
--- test/controllers/adverts_controller_test.rb
+++ test/controllers/adverts_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AdvertsControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = users(:piotr)
+  end
+  
   test "should get new" do
+    login_as(@user)
     get :new
     assert_response :success
   end
@@ -9,6 +15,15 @@ class AdvertsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+  end
+  
+  test "new advert can be created only by logged in users" do
+  end
+  
+  test "logged out user should be redirected" do
+    get :new
+    assert_not flash.empty?
+    assert_redirected_to login_path
   end
   
 end
